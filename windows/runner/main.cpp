8db3339d5a4cfababb62cd4acd049330b844022f
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2022-01-13 01:09:40 -0300

    Changing window name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -28,7 +28,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   Win32Window::Point origin(10, 10);
   //Win32Window::Size size(1280, 720); //Original value
   Win32Window::Size size(800, 720); //TODO: Limit window on flutter instead of C++.
-  if (!window.CreateAndShow(L"texture_channel_packer", origin, size)) {
+  if (!window.CreateAndShow(L"Texture Channeller", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
